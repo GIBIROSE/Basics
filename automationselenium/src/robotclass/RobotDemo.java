84b0a99f6e0d3af75eae9244aee5568379897d9a
Gibi Rose Mathew <gibirose.rose@gmail.com>
2022-04-07 18:57:17 +0530

    n

--- automationselenium/src/robotclass/RobotDemo.java
+++ automationselenium/src/robotclass/RobotDemo.java
@@ -1,15 +1,20 @@
 package robotclass;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RobotDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, AWTException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\seleniums\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://www.qabible.in/payrollapp/");
 		driver.findElement(By.id("loginform-username")).click();
 		driver.findElement(By.id("loginform-username")).sendKeys("carol");
@@ -18,6 +23,22 @@ public class RobotDemo {
 		driver.findElement(By.id("loginform-password")).sendKeys("1q2w3e4r");
 
 		driver.findElement(By.xpath("//button[@name='login-button']")).click();
+
+		Thread.sleep(2000);
+
+		// driver.findElement(By.linkText("Invoice")).click();
+
+		driver.findElement(By.xpath("//a[@href='/payrollapp/invoice/index']")).click();
+
+		driver.findElement(By.xpath("(//span[@class='glyphicon glyphicon-paperclip'])[1]")).click();
+
+		Robot robot = new Robot();
+
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_S);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_S);
+		robot.keyPress(KeyEvent.VK_ENTER);
 
 	}
 
